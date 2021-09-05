6728de02c041e908a4373167cf512ff359c77859
OmarGUEDDARI <48791037+OmarGUEDDARI@users.noreply.github.com>
2021-09-05 09:46:05 +0200

    Update projects.py

--- projects.py
+++ projects.py
@@ -1,3 +1,5 @@
 import pandas
 import numpy
 import streamlit
+Projects=dict({'Project_Name':['After Sales Analysis','Object Detection'] ,'Tags' : ['NLP','DL']})
+streamlit.selectbox('Projects',options=Projects['Project_Name'])
