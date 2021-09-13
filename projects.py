5cf24e98fab18e0df0abbbbf5919b45c1e4a89be
OmarGUEDDARI <48791037+OmarGUEDDARI@users.noreply.github.com>
2021-09-13 20:45:47 +0200

    Update projects.py

--- projects.py
+++ projects.py
@@ -1,5 +1,42 @@
 import pandas
 import numpy
 import streamlit
-Projects=dict({'Project_Name':['After Sales Analysis','Object Detection'] ,'Tags' : ['NLP','DL']})
-streamlit.selectbox('Projects',options=Projects['Project_Name'])
+import mysql.connector
+import pyspark
+from pyspark import SparkContext
+from apiclient.discovery import build
+connexion=mysql.connector.connect(user='sql11436700', database='sql11436700',password='rPVRabqddG',port='3306',host='sql11.freesqldatabase.com')
+Projects=dict({'Project_Name':['Data Science','Youtube Comments','Object Detection'] ,'Tags' : ['Discovery','NLP','DL']})
+ML_Steps=['Preprocessing','Feature Selection','Model Selection','Model Tuning','Deployment']
+step=''
+url=''
+youtube = build('youtube','v3',
+                developerKey="AIzaSyBTPzZunEN9bWskxKh2xoUvBreUoB4QHhk")
+comments=''
+iterator=1
+def youtube_extraction(url):
+    global comments
+    global iterator
+    sc = SparkContext()
+    comments=sc.emptyRDD()
+    video_id=url.split('?v=')[1][0:11]
+    video_response=youtube.commentThreads().list(
+    part='snippet,replies',
+    videoId=video_id).execute()
+    while iterator<100 :
+      #while video_response :  
+        for item in video_response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comments = comments.union(sc.parallelize(comment))
+            print(comment)
+            iterator+=1
+    return comments 
+    sc.stop()
+streamlit.button('Hi There Welcome to This App about Data Science Click on this Button to Start!!')
+Project=streamlit.selectbox('Projects',options=Projects['Project_Name'])
+if 'Data' in Project :
+    step=streamlit.radio(label='Steps',options=ML_Steps)
+if 'Comments' in Project :
+    url=streamlit.text_input("Youtube Video URL", '')
+    youtube_extraction(url)
+    streamlit.write(1)
